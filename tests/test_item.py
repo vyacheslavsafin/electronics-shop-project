d1f2beb22d98c9e2363d7256e9c9f410fd5534f6
vyacheslavsafin <safin.vr@mail.ru>
2023-06-30 00:05:46 +0300

    Реализованы тесты

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    item1.calculate_total_price()
+    assert item1.price * item1.quantity == 200000
+    assert item2.price * item2.quantity == 100000
+
+
+def test_apply_discount():
+    item1 = Item("Смартфон", 10000, 20)
+
+    item1.apply_discount()
+    assert item1.price == 8000.0
